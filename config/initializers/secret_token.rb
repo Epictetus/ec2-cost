647fb3e90822759de9a8315b5e4b21c9f982f612
u16suzu <u16suzu@gmail.com>
2011-12-11 23:17:14 +0900

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ec2Cost::Application.config.secret_token = '812bd84d23035a23813183690c54733f30e2a6982a1705c2a8000e561b15323affe7cb35b4e7806d47225bd51805e7cfeb59854e59eb4c330ae5e6ddb336101a'
